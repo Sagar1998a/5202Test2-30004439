55a9057b3814ccf66ebe6ad6afeeced8ee39ef00
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2018-09-27 14:55:43 +1200

    bird

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ sagar devkota 
+ student Id=30004439
+  */using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        get {return name; }
+    set {name ,=Value;}
+
+
+
+}
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter bird name: ");
+            string bird = Console.ReadLine();
+            Console.Write("Please enter species name: ");
+            string species = Console.ReadLine();
+            Console.Write("Please enter sex name: ");
+            string sex = Console.ReadLine();
+            Console.Write("Please enter weight name: ");
+            string weight= Console.ReadLine();
+            Console.Write("Please enter bandno name: ");
+            string bandno = Console.ReadLine();
+
+
+
+        }
+    }
+}
